2dc292917de04d9824c8d718587801e356dda748
Srinivas Damam <srinivas_damam@intuit.com>
2023-11-26 14:06:54 +0530

    Initial Commit

---
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,16 @@
+package com.kotlinplayground.basics
+
+/**
+ * NOTES:
+ * Use val for immutable variables
+ * Use var for mutable variables
+ */
+fun main() {
+    val name: String = "Sree"
+    println(name);
+
+    var city: String = "Bengaluru"
+    city = "Pune"
+    println(city)
+
+}
