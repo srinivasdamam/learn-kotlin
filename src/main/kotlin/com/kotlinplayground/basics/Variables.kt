315036e7a692b5660c42be00db086f7c5ee89eb4
Srinivas Damam <srinivas_damam@intuit.com>
2023-11-26 14:23:49 +0530

    String interpolation

--- src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -7,10 +7,18 @@ package com.kotlinplayground.basics
  */
 fun main() {
     val name: String = "Sree"
-    println(name);
+    println("Name is $name")
 
     var city: String = "Bengaluru"
     city = "Pune"
-    println(city)
+    println("City is $city")
 
+    // Types are automatically inferred by compiler
+    // when initialisation is done with declaration
+    val salary = 30000L;
+    println("Salary is $salary")
+
+    // String interpolation is done by :$
+    val age = 30;
+    println("Age is $age")
 }
