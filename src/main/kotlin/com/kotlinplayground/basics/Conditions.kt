1d638ca28d5dd403c4a0d87450922d45444d5704
Srinivas Damam <srinivas_damam@intuit.com>
2023-11-26 15:34:05 +0530

    When

--- src/main/kotlin/com/kotlinplayground/basics/Conditions.kt
+++ src/main/kotlin/com/kotlinplayground/basics/Conditions.kt
@@ -1,2 +1,12 @@
 package com.kotlinplayground.basics
 
+fun main() {
+    var position = 1
+    val result = when(position) {
+        1 -> "GOLD"
+        2 -> "SILVER"
+        3 -> "BRONZE"
+        else -> "NO MEDAL"
+    }
+    println(result)
+}
